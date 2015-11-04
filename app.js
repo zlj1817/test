6518f2e8494a131522e2ee3f3dfe3271b23fa517
zlj1817 <zlj1817@163.com>
2015-11-04 15:58:29 +0800

    创建一个http server

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 console.log("app start");
-var a=3;
-a=5;
-	a="hello world";
-	console.log(a);
+var http=require('http');
+function callback(req,res){
+res.writeHead(200,{'Content-Type':'text/plain'});
+//res.write("start<br>");
+res.end("hello world")
+}
+http.createServer(callback).listen(8888);
+console.log('server run');
